0e39365008d7aa685d04c6fa1c911abb84568fb2
Chad Remesch <chad@remesch.com>
2014-03-13 10:53:20 -0400

    Generator for migrations.

--- lib/totem_activerecord/shell_cmds/db.rb
+++ lib/totem_activerecord/shell_cmds/db.rb
@@ -3,10 +3,13 @@ module TotemActiverecord
     class Db < Totem::ShellCmds::Base
       def run
         case @args[0]
-        when 'create'   then create
-        when 'drop'     then drop
-        when 'migrate'  then migrate
-        when 'rollback' then rollback
+        when 'create'    then create
+        when 'drop'      then drop
+        when 'migrate'   then migrateb
+        when 'rollback'  then rollback
+        when 'migration' then migration(@args[1])
+        else
+          puts_usage
         end
       end
 
@@ -38,6 +41,44 @@ module TotemActiverecord
         TotemActiverecord.reconnect
 
         return true
+      end
+
+      def migration(name)
+        if name.length == 0
+          puts "ERROR: You must provide a name for the migration."
+          puts
+          puts_usage
+          return
+        end
+
+        timestamp =  Time.now.utc.strftime("%Y%m%d%H%M%S")
+        filename = "#{timestamp}_#{name}.rb"
+        path = File.join(Totem.root, 'db', 'migrate', filename)
+        content = "class #{name.camelize} < ActiveRecord::Migration\nend"
+
+        puts "Creating migration: #{path}"
+
+        if File.exists?(path)
+          puts "ERROR: Migration already exists."
+          return false
+        end
+
+        File.open(path, 'w') { |f| f.write(content) }
+
+        return true
+      end
+
+      private
+
+      def puts_usage
+        puts "Usage:\n  bundle exec totem db <command>"
+        puts
+        puts "Commands:\n"
+        puts "  create           - Create the database if it doesn't exist."
+        puts "  drop             - Drop the database if it exists."
+        puts "  migrate          - Run all new migrations."
+        puts "  rollback         - Rollback one migration."
+        puts "  migration <name> - Create a new migration for the given name."
       end
     end
   end
